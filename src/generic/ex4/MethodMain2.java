d3fc7ee7139b278e15402c935f7b0d17bc5dcbdc
cops9080 <up93py@gmail.com>
2025-07-17 16:54:54 +0900

    mogakco

---
+++ src/generic/ex4/MethodMain2.java
@@ -0,0 +1,9 @@
+package generic.ex4;
+
+import generic.animal.Dog;
+
+public class MethodMain2 {
+    public static void main(String[] args) {
+        Dog dog = new Dog("멍멍이", 100);
+    }
+}
---
+++ src/prac/MethodMain1.java
@@ -0,0 +1,24 @@
+package prac;
+
+public class MethodMain1 {
+    public static void main(String[] args) {
+        Integer i = 10;
+
+        Object object = GenericMethod.objMethod(i);
+        Integer integer = (Integer) GenericMethod.objMethod(i); //캐스팅 필요
+
+        //타입 인자 명시적 전달
+        System.out.println("명시적 타입 인자 전달");
+        Integer result = GenericMethod.<Integer>genericMethod(i);
+        Integer integerValue = GenericMethod.<Integer>numberMethod(10);
+        Double doubleValue = GenericMethod.<Double>numberMethod(20.0);
+
+        //타입 추론
+        System.out.println("타입 추론");
+        Integer result1 = GenericMethod.genericMethod(i);
+        Integer integerValue1 = GenericMethod.numberMethod(10);
+        Double doubleValue1 = GenericMethod.numberMethod(20.0);
+
+
+    }
+}
